a0e7c5e9cf49f455689a1f806fe4f59b3863228d
DESKTOP-J2DM1UE\Michael <michaelbarnes7282>
2020-06-17 14:59:20 -0700

    finished mvp

--- enhancing/enhancer.js
+++ enhancing/enhancer.js
@@ -6,15 +6,38 @@ module.exports = {
 };
 
 function succeed(item) {
-  return { ...item };
+  if(item.enhancement === 20){
+    return { ...item };
+  } else {
+    item.enhancement += 1;
+    return { ...item };
+  }
 }
 
 function fail(item) {
-  return { ...item };
+  if (item.enhancement >= 15) {
+    item.durability -= 10
+    if (item.enhancement > 16) {
+      item.enhancement--;
+      console.log('over 15 fail', item);
+    }
+    console.log('equal or over fail', item)
+    return { ... item }
+  } else if (item.enhancement < 15) {
+    item.durability -= 5;
+    if (item.durability < 0){
+      item.durability = 0;
+    }
+    console.log('under fail', item)
+    return { ...item };
+  }
 }
 
 function repair(item) {
-  return { ...item };
+  item.durability = 100;
+  return {
+    ...item,
+  };
 }
 
 function get(item) {
--- enhancing/enhancer.spec.js
+++ enhancing/enhancer.spec.js
@@ -1,2 +1,69 @@
 const enhancer = require('./enhancer.js');
 // test away!
+
+describe('enhancer', () => {
+    const sword = {
+        name: "iron sword",
+        enhancement: 0,
+        durability: 50
+    }
+
+    const staff = {
+        name: 'staff of fire',
+        enhancement: 19,
+        durability: 30
+    }
+
+    const bow = {
+        name: 'bow of life',
+        enhancement: 15,
+        durability: 100
+    }
+
+    describe('repair', () => {
+        test('should bring item durability to 100', () => {
+            enhancer.repair(sword);
+            enhancer.repair(staff)
+            expect(sword.durability).toBe(100);
+            expect(staff.durability).toBe(100);
+        });
+    });
+
+    describe('success', () => {
+        test('should bring item enhancement up by 1 if not already at 20', () => {
+            enhancer.succeed(staff);
+            enhancer.succeed(staff);
+            enhancer.succeed(sword);
+
+            expect(sword.enhancement).toBe(1);
+            expect(staff.enhancement).toBe(20);
+        });
+    });
+
+    describe('failed e > 16', () => {
+        test('should reduce item enhancement by 1 and dura by 10 if enhancement > 16', () => {
+            enhancer.fail(staff);
+
+            expect(staff.durability).toBe(90);
+            expect(staff.enhancement).toBe(19);
+        });
+    });
+
+    describe('failed e > 15', () => {
+        test('should reduce item durability by 10 if enh > 15', () => {
+            enhancer.fail(bow);
+
+            expect(bow.durability).toBe(90);
+            expect(bow.enhancement).toBe(15);
+        });
+    });
+
+    describe('failed e < 15', () => {
+        test('should reduce item durability by 5 if enh < 15', () => {
+            enhancer.fail(sword);
+
+            expect(sword.durability).toBe(95);
+            expect(sword.enhancement).toBe(1);
+        });
+    });
+});
